384a4ffa098311ef8824dd92e8272221366c75be
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-13 21:35:07 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -47,10 +47,7 @@ const getOrders = async (req, res, next) => {
   try {
     const orders = await Order.find({ userId });
 
-    res.status(200).json({
-      success: true,
-      orders,
-    });
+    res.status(200).json(orders);
   } catch (error) {
     next(error);
   }
