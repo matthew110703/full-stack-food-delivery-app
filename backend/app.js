104ae741f4a5da1a1a90c56004e42767a16bb4a4
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-13 12:44:38 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const logger = require("morgan");
 
 const app = express();
+
+// Middleware
+app.use(logger("dev"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
+
+// Global error handler
+const errorHandler = require("./middleware/errorHandler");
+app.use(errorHandler);
 
 module.exports = app;
