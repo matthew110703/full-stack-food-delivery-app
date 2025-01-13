090050d72d5c83b79203d656ba4e42e8a1ed7e91
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-13 21:24:44 +0530

    Update authenticate.js

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -3,12 +3,14 @@ const jwt = require("jsonwebtoken");
 /** @description Authentication Middleware */
 const authenticate = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.toString().split(" ")[1];
-    if (!token) {
+    const bearerToken = req.headers.authorization;
+    if (!bearerToken) {
       const error = new Error("Unauthorized. Access token is required.");
       error.statusCode = 401;
       return next(error);
     }
+
+    const token = bearerToken.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
