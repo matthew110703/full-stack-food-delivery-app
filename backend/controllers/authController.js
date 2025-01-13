a418f7d6446404bfe11e03ff87693c561b41f0a9
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-13 13:36:47 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,42 +4,14 @@ const bcrypt = require("bcrypt");
 // Model
 const User = require("../models/userModel");
 
-/** Req Payload Validation */
-const validate = (username, password) => {
-  if (!username || !password) {
-    const error = new Error("Username and password are required");
-    error.statusCode = 400;
-    return error;
-  }
-
-  if (password.length < 8) {
-    const error = new Error("Password must be at least 6 characters long");
-    error.statusCode = 400;
-    return error;
-  }
-
-  if (username.length < 3) {
-    const error = new Error("Username must be at least 3 characters long");
-    error.statusCode = 400;
-    return error;
-  }
-
-  if (username.length > 20) {
-    const error = new Error("Username must be at most 20 characters long");
-    error.statusCode = 400;
-    return error;
-  }
-
-  return;
-};
-
 /** @description Reagister a new user */
 const register = async (req, res, next) => {
   const { username, password } = req.body;
 
   // Validate request payload
-  const error = validate(username, password);
-  if (error) {
+  if (!username || !password) {
+    const error = new Error("Username and password are required");
+    error.statusCode = 400;
     return next(error);
   }
 
@@ -69,6 +41,13 @@ const register = async (req, res, next) => {
 const login = async (req, res, next) => {
   const { username, password } = req.body;
 
+  // Validate request payload
+  if (!username || !password) {
+    const error = new Error("Username and password are required");
+    error.statusCode = 400;
+    return next(error);
+  }
+
   try {
     // Check if user exists
     const user = await User.findOne({ username });
@@ -84,7 +63,7 @@ const login = async (req, res, next) => {
     if (!isValidPassword) {
       const error = new Error("Invalid password");
       error.statusCode = 401;
-      next(error);
+      return next(error);
     }
 
     // Create token
