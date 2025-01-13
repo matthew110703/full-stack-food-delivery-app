e4a19231d02244938a8107c54967f82dee071154
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-13 14:16:14 +0530

    Added getMenuItem Route Handler

--- backend/controllers/menuController.js
+++ backend/controllers/menuController.js
@@ -11,6 +11,25 @@ const getAllMenuItems = async (req, res, next) => {
   }
 };
 
+/** @description Get a single menu item */
+const getMenuItem = async (req, res, next) => {
+  const { id } = req.params;
+
+  // Validation
+  if (!id) {
+    const error = new Error("Menu item ID is required");
+    error.status = 400;
+    return next(error);
+  }
+
+  try {
+    const menuItem = await Menu.findById(id);
+    res.json({ menuItem });
+  } catch (error) {
+    next(error);
+  }
+};
+
 /** @description Add a new menu item */
 const addMenuItem = async (req, res, next) => {
   const { name, category, price, availability } = req.body;
@@ -75,6 +94,7 @@ const deleteMenuItem = async (req, res, next) => {
 
 module.exports = {
   getAllMenuItems,
+  getMenuItem,
   addMenuItem,
   updateMenuItem,
   deleteMenuItem,
