227d1b742769f332cf11b0a2e6f1bfdde64f137a
unknownloop11 <unknownloop11@gmail.com>
2025-01-13 11:50:24 +0530

    Database Connection

--- backend/lib/dbConnect.js
+++ backend/lib/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/express-mongo");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected");
   } catch (error) {
     console.log("Database connection failed", error);
