8cc275234ea20a7c9108f53fae299be4c854b948
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-13 13:19:12 +0530

    Enhanced Validation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,9 +4,44 @@ const bcrypt = require("bcrypt");
 // Model
 const User = require("../models/userModel");
 
+/** Req Payload Validation */
+const validate = (username, password) => {
+  if (!username || !password) {
+    const error = new Error("Username and password are required");
+    error.statusCode = 400;
+    return error;
+  }
+
+  if (password.length < 8) {
+    const error = new Error("Password must be at least 6 characters long");
+    error.statusCode = 400;
+    return error;
+  }
+
+  if (username.length < 3) {
+    const error = new Error("Username must be at least 3 characters long");
+    error.statusCode = 400;
+    return error;
+  }
+
+  if (username.length > 20) {
+    const error = new Error("Username must be at most 20 characters long");
+    error.statusCode = 400;
+    return error;
+  }
+
+  return;
+};
+
 /** @description Reagister a new user */
 const register = async (req, res, next) => {
   const { username, password } = req.body;
+
+  // Validate request payload
+  const error = validate(username, password);
+  if (error) {
+    return next(error);
+  }
 
   try {
     // Check if user already exists
@@ -14,7 +49,7 @@ const register = async (req, res, next) => {
     if (userExists) {
       const error = new Error("User already exists");
       error.statusCode = 400;
-      next(error);
+      return next(error);
     }
 
     // Hash password
