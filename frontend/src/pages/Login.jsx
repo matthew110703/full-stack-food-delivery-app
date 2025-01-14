7f47a72b51a55c3eb0dc30002570c23663796d55
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-14 16:44:24 +0530

    Implemented login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,83 @@
+// React
+import { useState } from "react";
+
+// UI Components
+import { Container, Link } from "../components";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+  };
+
+  return (
+    <Container>
+      <form className=" max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-4 w-full">
+          <h1 className="text-center text-2xl font-bold mb-6">
+            Sign in to your account
+          </h1>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            autoComplete="username"
+            required
+            className="mt-1 p-2.5 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            placeholder="john_doe"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="current-password"
+            required
+            className="mt-1 p-2.5 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            placeholder="••••••••"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full p-2.5 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Sign in
+        </button>
+
+        <div className="mt-4 text-center">
+          <div className="text-sm">
+            Don&apos;t have an account?{" "}
+            <Link
+              href="/signup"
+              className="underline font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </form>
+    </Container>
+  );
 };
 
 export default Login;
