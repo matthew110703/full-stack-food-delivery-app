e1f43d042c29e7705d3d520e8a489caa2dae2f03
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-14 13:02:40 +0530

    Added CORS

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 
