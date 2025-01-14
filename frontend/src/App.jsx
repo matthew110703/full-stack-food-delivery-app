5fbaafa9f86f87f2a38a7c0d4e6a6edbcf37c97a
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-14 15:13:05 +0530

    Initialized Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+// React Imports
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Pages
+import { Home, Login, Order, SignUp } from "./pages";
+
 const App = () => {
-  return <div className="text-4xl">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
