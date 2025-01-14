4d03828a0e7f9cde0ce933cdb768c25fb4c1857b
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-14 16:00:48 +0530

    Create Button.jsx

---
+++ frontend/src/components/ui/Button.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Button = ({ href, className, title, onClick }) => {
+  return (
+    <Link
+      role="button"
+      className={`px-3 py-1.5 bg-blue-500 border-white shadow hover:bg-blue-800 hover:scale-105 rounded-md text-sm text-white font-semibold active:scale-95 transition-all ${className}`}
+      to={href}
+      onClick={onClick}
+    >
+      {title}
+    </Link>
+  );
+};
+
+Button.propTypes = {
+  href: PropTypes.string,
+  className: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default Button;
